93cbfcdf181d2e347b460cc756645e6316fa5bf8
Micael Souza <micael.souza@outlook.com>
2015-05-23 10:35:19 -0300

    next step

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,7 +1,25 @@
-var HelloMessage = React.createClass({
-  render: function() {
-    return <div>Hello {this.props.name}</div>;
-  }
+var BoxMessages = React.createClass({
+	render: function() {
+		<section id="wc_box_messages"></section>;
+	};
 });
+
+var ListUsers = React.createClass({
+	render: function() {
+		return <section id="wc_list_users"></section>;
+	}
+})
+
+var ReactChat = React.createClass({
+	render: function() {
+		return (
+			<section id="{this.props.id}">
+				<ListUsers/>
+				<BoxMessages/>
+			</section>
+		);
+	}
+})
+
 var mountNode = document.getElementById('mountNode');
-React.render(<HelloMessage name="John" />, mountNode);
+React.render(<ReactChat id="WiChatMuhahaha" />, mountNode);
