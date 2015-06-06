ca10c2bab1390e67b87744c6e46c10607d07efeb
Michel Cavalcante <hnri_mxel@hotmail.com>
2015-06-06 06:10:57 -0300

    Dojo round: 2 - My Round: 1

--- app.js
+++ app.js
@@ -2,48 +2,77 @@
 
 var _ = require('lodash');
 var socket = require('socket.io');
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var cfg = require('./config.json');
 var chalk = require('chalk');
 
-function init(participants) {
+function* participant(participants){
+	var round = 1;
 	var backlog = addParticipantsToBacklog(participants);
-	var participant = nextParticipant(backlog, participants);
-}
-
-function nextParticipant(backlog, participants) {
-	let pilot = backlog.shift();
-
-	process.stdout.write('\x1B[2J');
-	console.log(chalk.green('Pilot:'));
-	console.log(pilot);
-	console.log(chalk.green('Co-pilot: '));
-	console.log(backlog[0]);
-	console.log(chalk.green('Get ready: '));
-	console.log(backlog[1]);
-	
-	if (backlog.length <= participants.length) {
-		backlog = addParticipantsToBacklog(participants, backlog);
+	while(true) {
+		if (backlog.length <= participants.length) {
+			backlog = addParticipantsToBacklog(participants, backlog);
+		}
+		let pilot = backlog.shift();
+		pilot.round = round++;
+		yield {
+			pilot: pilot,
+			coPilor: backlog[0],
+			getReady: backlog[1]
+		};
 	}
+}
+var participant = participant(_.where(cfg.participants, {"active": true}));
 
-	setTimeout(function () {
-		pushCurrentCode(backlog, participants);
+function init(participant) {
+	var beforePilot = participant.next().value.pilot;
+	var timer = setInterval(function () {
+		pushCurrentCode(beforePilot);
+
+		let actualParticipant = participant.next().value;
+		console.log(chalk.green('Actual Participants on desk:'));
+		console.log(actualParticipant);
+		beforePilot = actualParticipant.pilot;
 	}, cfg.time);
+
 }
 
-function pushCurrentCode(backlog, participants) {
-	//Do commits here
-	nextParticipant(backlog, participants);
+function pushCurrentCode(pilot) {
+    
+	exec('git config user.name "' + pilot.username + '" --replace-all');
+	exec('git config user.email ' + pilot.email + ' --replace-all');
+	exec('git add --all');
+	let message = 'Dojo round: ' + pilot.round + ' - My Round: ' + Math.ceil(pilot.round/4);
+	console.log(chalk.grey(message));
+	exec('git commit -m "' + message + '"');
+
+	console.log(chalk.green('Code to ' + pilot.username + ' has been commited.'));
+	console.log(pilot);
+	
 }
+
+function handleChildProcess(error, stdout, stderr) {
+   if (error) {
+     console.log(error.stack);
+     console.log('Error code: '+error.code);
+     console.log('Signal received: '+error.signal);
+   }
+   console.log('Child Process STDOUT: '+stdout);
+   console.log('Child Process STDERR: '+stderr);
+ }
 
 function addParticipantsToBacklog(participants, backlog) {
+
 	if (!backlog) {
 		backlog = _.shuffle(participants);
 	}
+
 	do {
 		participants = _.shuffle(participants)
 	} while (_.last(backlog) === _.first(participants));
+	
 	return backlog.concat(participants);
+
 }
 
-init(_.where(cfg.participants, {"active": true}));
+init(participant);
