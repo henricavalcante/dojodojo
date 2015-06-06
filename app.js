3a440d09e55f28a5fa4c083712b71d070f7a128e
Henri <hnri_mxel@hotmail.com>
2015-06-06 06:45:19 -0300

    Dojo round: 68 - My Round: 17

--- app.js
+++ app.js
@@ -12,10 +12,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-	io.emit('round', 'Connected...');
-});
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
